7fbaf973e926358af303316b59060d4991f8a9d1
hoangthanh <phucthanh290803@gmail.com>
2022-10-04 20:34:56 +0700

    JavaCore

--- src/main/java/Buoi_3/SwichCase_Cong_Tru_Nhan_Chia.java
+++ src/main/java/Buoi_3/SwichCase_Cong_Tru_Nhan_Chia.java
@@ -35,6 +35,6 @@ public class SwichCase_Cong_Tru_Nhan_Chia {
             default:
                 System.out.println("Nhap sai");
         }
-        System.out.println();
+        System.out.println("sds");
     }
 }
