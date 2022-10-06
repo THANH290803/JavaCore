72e326c92b0990829e0b8b1b07766d0aa92e31e9
hoangthanh <phucthanh290803@gmail.com>
2022-10-06 18:45:17 +0700

    BTVN

--- src/main/java/Buoi_3/BTVN/BTVN2.java
+++ src/main/java/Buoi_3/BTVN/BTVN2.java
@@ -5,21 +5,28 @@ import java.util.Scanner;
 public class BTVN2 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String choose;
+        String choose = "Yes";
         do {
-                int i = 1, n;
-                System.out.println("Nhap so: ");
-                n = input.nextInt();
+            int i = 1, n;
+            System.out.println("Nhap so: ");
+            n = input.nextInt();
 
+            if (n > 9 || n < 0) {
+                System.out.println("Nhap lai");
+                continue;
+            } else if (n == 0) {
+                break;
+            } else {
                 while (i <= 10) {
                     System.out.println(i + "*" + n + "=" + i * n);
                     i++;
                 }
-                System.out.println("Do you want to continues?(Y/N)");
-                input.nextLine();
-                choose = input.nextLine();
-
-        } while (choose.equalsIgnoreCase("Yes"));
+            }
+            System.out.println("Do you want to continues?(Y/N)");
+            input.nextLine();
+            choose = input.nextLine();
+        } while (choose.equals("Yes")) ;
         input.close();
     }
 }
+
