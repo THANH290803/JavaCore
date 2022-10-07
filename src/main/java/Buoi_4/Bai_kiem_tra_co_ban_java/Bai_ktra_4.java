9730b19c316a3d006be73193d311672141a90061
hoangthanh <phucthanh290803@gmail.com>
2022-10-08 02:31:08 +0700

    Bài ktra java cơ bản

--- src/main/java/Buoi_4/Bai_kiem_tra_co_ban_java/Bai_ktra_4.java
+++ src/main/java/Buoi_4/Bai_kiem_tra_co_ban_java/Bai_ktra_4.java
@@ -9,7 +9,7 @@ public class Bai_ktra_4 {
         if (n < 2) {
             return false;
         }
-        // check so nguyen to khi n >= 2
+
         int squareRoot = (int) Math.sqrt(n);
         for (int i = 2; i <= squareRoot; i++) {
             if (n % i == 0) {
