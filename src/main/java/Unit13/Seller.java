33be0c4255a1000186f8a21572505c67e401d4ce
hoangthanh <phucthanh290803@gmail.com>
2022-10-27 19:48:26 +0700

    Buổi 13

---
+++ src/main/java/Unit13/Seller.java
@@ -0,0 +1,23 @@
+package Unit13;
+
+public class Seller extends Employee{
+    private double revenue;
+
+    public Seller() {
+    }
+
+    public Seller(String name, double revenue, double salary) {
+        super(name, salary);
+        this.revenue = revenue;
+    }
+
+
+    public double getSalary() {
+        if (revenue < 4000){
+            super.setSalary(super.getSalary() + (0.05*revenue));
+        } else {
+            super.setSalary(super.getSalary() + (0.1 * revenue));
+        }
+        return super.getSalary();
+    }
+}
